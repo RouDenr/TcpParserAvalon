a87272f796f8d1a5baf4980c00f97afc2b15fcbc
decordel <mrroudenr@gmail.com>
2024-01-21 07:31:13 +0300

    docs: sample console commands

---
+++ ServerModel/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// Основыные команды
+// help - список команд
+// clients - список клиентов
+// client <id> - информация о клиенте
+// client <id> <command> - выполнение команды на клиенте
+// parse <path> - парсинг файла
+// send <user/all> <path> - отправка файла
+
+// exit - выход
