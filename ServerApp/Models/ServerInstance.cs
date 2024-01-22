7fdef702dedb28a98a16f46ba666d58a2fb033b0
decordel <mrroudenr@gmail.com>
2024-01-22 22:57:51 +0300

    feat: add server instance

---
+++ ServerApp/Models/ServerInstance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using ServerModel.XmlParser.ClientModel;
+using ServerModel.XmlParser.Data;
+using ServerModel.XmlParser.Server;
+
+namespace ServerApp.Models;
+
+public class ServerInstance
+{
+	public XmlParserServer ParseServer => Server as XmlParserServer ?? throw new NullReferenceException("Server is null");
+	public TcpClientHandler TcpClients => Clients as TcpClientHandler ?? throw new NullReferenceException("Clients is null");
+	
+	private IServer Server { get; }
+	private IClientHandler Clients { get; }
+	
+	
+	
+	public static ServerInstance Instance { get; } = new ();
+
+	private ServerInstance()
+	{
+		XmlDataFactory factory = new ();
+		Clients = new TcpClientHandler(port: 8888);
+		Server = new XmlParserServer(Clients, factory.CreateDataProcessor(), factory.CreateParser());
+	}
+
+	public XmlData ParseFile(FileInfo file)
+	{
+		return (XmlData) ParseServer.ParseFile(file);
+	}
+}
